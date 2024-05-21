c6aa9a532a20ae47bbf9dd4fa0ba3a4bcc1cfaf9
Kleber-sjc <158180628+Kleber-sjc@users.noreply.github.com>
2024-05-21 20:05:23 -0300

    meu projeto banco

--- canta-banco/src/ContaTerminal.java
+++ canta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+       //TODO:Conhecer e importar a casse Scaner
+
+       //Exibir as mensagens para o nosso usuário
+
+       //Obter pala scaner os valores digitados no terminal
+
+       //Exibir a mensagem conta criada
+       System.out.println("\n==== Olá!, seja bem vindo ao Banco DIO. ====\n");
+    
+       float saldo = 237.48f;
+       //int conta = teclado.nextInt();
+       //int agencia = teclado.nextInt();
+       //String nome= teclado.nextLine();
+
+
+       System.out.print( "Digite o nome de usuário: ");
+       String nome = teclado.nextLine();
+       System.out.print( "Digite o número da agência: ");
+       int agencia = teclado.nextInt();
+       System.out.print( "Digite o número da conta ");
+       int conta = teclado.nextInt();
+       System.out.printf("\nO saldo em conta é R$"+saldo);
+       System.out.println("\nOlá! "+nome+",obrigado por criar uma conta em nosso banco, sua agência é "+agencia+ ",conta "+conta+" e seu saldo "+saldo+" já está disponivel para saque.\n\n");
+
+
+
     }
 }
